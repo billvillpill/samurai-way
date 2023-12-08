d364a9cc9a2836715ca6c1f9947c58cec39363b1
billvillpill <belarus0000@gmail.com>
2023-12-08 14:26:23 +0300

    исправил ошибку в import module css Dialogs.tsx

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -3,7 +3,7 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
+        <div className={s.conteiner}>
             Dialogs
         </div>
     );
