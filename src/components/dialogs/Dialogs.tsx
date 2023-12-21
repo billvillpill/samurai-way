e5c7c906f14757c1c4e5787a1f6470b42f4a4158
billvillpill <belarus0000@gmail.com>
2023-12-21 20:44:26 +0300

    добавил диалоги

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -4,7 +4,33 @@ import s from './Dialogs.module.css';
 export const Dialogs = () => {
     return (
         <div className={s.conteiner}>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        Dmitry
+                    </div>
+                    <div className={s.dialog}>
+                        Andrey
+                    </div>
+                    <div className={s.dialog}>
+                        Sveta
+                    </div>
+                    <div className={s.dialog}>
+                        Sasha
+                    </div>
+                    <div className={s.dialog}>
+                        Victor
+                    </div>
+                    <div className={s.dialog}>
+                        Valera
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi!</div>
+                    <div className={s.message}>How is your it-kamasutra?</div>
+                    <div className={s.message}>Yo!</div>
+                </div>
+            </div>
         </div>
     );
 };
